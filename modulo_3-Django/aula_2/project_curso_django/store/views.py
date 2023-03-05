bf42dd158e6f2d60ff113379f9eb15eaffc43cf4
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2023-03-05 15:53:34 -0300

    Adicionando as primeiras urls

--- modulo_3-Django/aula_2/project_curso_django/store/views.py
+++ modulo_3-Django/aula_2/project_curso_django/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def hello(request):
+    return render(request, "index.html")
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request,
+                "product/list.html",
+                { "products": products })
