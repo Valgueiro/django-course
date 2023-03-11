e0bffd1f4d1a303dc6783eb50b161db3f98c54f3
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2023-03-11 12:07:12 -0300

    Adiciona exemplos com funçoes

--- modulo_1-Python/exemplos/funcao.py
+++ modulo_1-Python/exemplos/funcao.py
@@ -35,7 +35,60 @@ def imprime():
     variavel_local = "local"
     print("Variavel local: ", variavel_local)
 
-imprime()
-print(variavel_local)
+# imprime()
+# print(variavel_local)
+
+
+def soma(a, b, imprime=False):
+    resultado = a + b
+    if imprime:
+        print(resultado)
+    return resultado
+
+
+soma(3, 5)
+# soma(3, 5, imprime=True)
+
+
+def computacao_assincrona(param1, param2, callbackFn):
+    try:
+        print("Fazendo uma computaçao assincrona bem complicada.....")
+    except:
+        print("Algo de errado aconteceu")
+        return None
+
+    return callbackFn(param1, param2)
+
+def callback(a, b):
+    return a + b
+
+print(computacao_assincrona(2, 4, callback))
+
+
+def multiplicacao(a, b):
+    return a*b
+
+argumentos = [4, 5]
+multiplicacao(*argumentos)
+
+
+
+def multiplicacao(*args):
+    acumulador = 1
+    for arg in args:
+        acumulador *= arg
+
+    return acumulador
+
+def fatorial(n):
+    return multiplicacao(*range(1, n))
+
+print(fatorial(6)) # 120
+
+multiplicacao(2, 3)
+multiplicacao(2, 3, 4, 5)
+
+
+
 
 
